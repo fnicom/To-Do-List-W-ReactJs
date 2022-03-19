554b63fe6423195d45c3acb1e5fdace9457675d0
fnicom <fnicomedes@gmail.com>
2022-03-19 10:07:34 -0300

    trabalhando com props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Comentarios from './components/Comentarios';
 function App() {
   return (
     <div className="App">
-      <h1>Todo LIst</h1>
-      <Comentarios/>
-      <Comentarios/>
-      <Comentarios/>
+      <h1>Todo List</h1>
+      <Comentarios nome={'John Do'} email='joao@mail.com' data={new Date(2020, 3, 19)}>
+        Olá, tudo bem?
+        </Comentarios>
+
+        <Comentarios nome='Maria' email='maria@mail.com' data={new Date(2020, 5, 22)}>
+        Olá, tudo bem?
+        </Comentarios>
     </div>
   );
 }
--- src/components/Comentarios.jsx
+++ src/components/Comentarios.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Comentario.css';
 
-const Comentarios = () => {
-    let nome = 'Maria';
+const Comentarios = (props) => {
+
     return (
         <div className='Comentarios'>
-            <div>Componentes de Comentarios</div>
-            <div>Olá meu nome é {nome}</div>
+            <h2>{props.nome}</h2>
+            <p>{props.email}</p>
+            <p>{props.children}</p>
+            <p>{props.data.toString()}</p>
         </div>
 
     )
